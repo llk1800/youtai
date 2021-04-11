22fc28ba02b90ccd1776e1aebb550438184c8984
liulk <llk1800@126.com>
2021-04-11 14:56:03 +0800

    fix:init

---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+return array(
+    'database' => array(
+        'type' => 'mysqli', // 数据库连接驱动类型: mysqli,sqlite,pdo_mysql,pdo_sqlite
+        'host' => '127.0.0.1', // 数据库服务器
+        'user' => 'root', // 数据库连接用户名
+        'passwd' => 'root', // 数据库连接密码
+        'port' => '3306', // 数据库端口
+        'dbname' => 'pbootcms' //数据库名称
+    )
+);
